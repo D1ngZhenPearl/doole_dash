aaf546b7a4c192c8c2f33a818915dffe9f641508
D1ngZhenPearl <middles_green.0y@icloud.com>
2023-01-12 17:58:11 +0800

    update git ignore file

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/wangweishan/dev/flutter-codelabs/flame-building-doodle-dash/step_02/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Users/wangweishan/dev/flutter-codelabs/flame-building-doodle-dash/step_02/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/Objects-normal/arm64/path_provider_macos-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/wangweishan/dev/flutter-codelabs/flame-building-doodle-dash/step_02/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-dummy.m \
-  /Users/wangweishan/dev/flutter-codelabs/flame-building-doodle-dash/step_02/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_macos.build/module.modulemap \
-  /Users/wangweishan/dev/flutter-codelabs/flame-building-doodle-dash/step_02/macos/Pods/Target\ Support\ Files/path_provider_macos/path_provider_macos-prefix.pch
